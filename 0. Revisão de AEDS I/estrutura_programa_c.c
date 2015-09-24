4acc216e631bd49ef528d4802a8ac9b0f7caf989
Ana Paula Gomes <apgomes88@gmail.com>
2015-09-24 15:16:03 -0300

    Estrutura do programa em C. Instrucoes pra compilacao.

---
+++ 0. Revisão de AEDS I/estrutura_programa_c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Isso eh um comentario. Referencia: Learn code the hard way http://c.learncodethehardway.org/book/ex5.html */
+/*
+Para compilar, no console (na mesma pasta em que esta o arquivo), digite:
+make estrutura_programa_c
+OU
+gcc estrutura_programa_c.c -o estrutura_programa_c
+
+Entao:
+./estrutura_programa_c
+
+*/
+int main(int argc, char *argv[]){
+
+    // Outro comentario
+    printf("Hello, World!\n");
+
+    return 0;
+}
