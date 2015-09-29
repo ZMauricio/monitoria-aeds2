4e63c5d9f62e1a37e215a0b0ba84c4acf503e61f
Ana Paula Gomes <apgomes88@gmail.com>
2015-09-29 15:29:24 -0300

    Exemplo simples de ponteiro

--- 0. Revisão de AEDS I/ponteiros.c
+++ 0. Revisão de AEDS I/ponteiros.c
@@ -2,10 +2,9 @@
 
 /*
 Um ponteiro eh uma variavel que ira guardar um endereco de memoria, ou seja, a referencia de onde o conteudo dessa variavel esta alocada.
-Ponteiros melhoram o desempenho de um programa. Por exemplo, ao passar uma variavel para uma funcao, eh feita uma copia dessa variavel, alocando memoria por duas vezes.
-Passando apenas o ponteiro dessa variavel, economizamos em memoria.
-Exemplo do livro C Completo e Total (capitulo 5)
+Referencia: http://www.ime.usp.br/~pf/algoritmos/aulas/pont.html
 */
+
 int main(int argc, char *argv[]){
 
     int variavel_qualquer;
@@ -15,6 +14,6 @@ int main(int argc, char *argv[]){
     ponteiro2 = ponteiro1;
 
     printf("%p", ponteiro2);
-
+    
     return 0;
 }
