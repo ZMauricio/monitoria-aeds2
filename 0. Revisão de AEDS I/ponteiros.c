da8b149bcb304886e6f6be3baf0dde5707cd76a8
Ana Paula Gomes <apgomes88@gmail.com>
2015-09-29 16:00:05 -0300

    Exemplo de troca de valores com ponteiros adicionado.

--- 0. Revisão de AEDS I/ponteiros.c
+++ 0. Revisão de AEDS I/ponteiros.c
@@ -5,6 +5,19 @@ Um ponteiro eh uma variavel que ira guardar um endereco de memoria, ou seja, a r
 Referencia: http://www.ime.usp.br/~pf/algoritmos/aulas/pont.html
 */
 
+void troca (int *p, int *q){
+   printf("Primeiro: %d\n", *p);
+   printf("Segundo: %d\n\n", *q);
+   int temp;
+   temp = *p;
+   *p = *q;
+   *q = temp;
+
+   printf("Primeiro: %d\n", *p);
+   printf("Segundo: %d\n", *q);
+
+}
+
 int main(int argc, char *argv[]){
 
     //Declaracao e atribuicao
@@ -14,10 +27,16 @@ int main(int argc, char *argv[]){
     ponteiro1 = &variavel_qualquer;//guarda o endereco da memoria onde esta essa variavel
     ponteiro2 = ponteiro1;
 
-    printf("%p", ponteiro2);
+    printf("%p\n\n", ponteiro2);
 
     //Troca de dois valores inteiros
+    int primeiro_numero = 1;
+    int segundo_numero = 2;
+    int *ponteiro_primeiro, *ponteiro_segundo;
+    ponteiro_primeiro = &primeiro_numero;
+    ponteiro_segundo = &segundo_numero;
 
+    troca(ponteiro_primeiro, ponteiro_segundo);
 
     return 0;
 }
